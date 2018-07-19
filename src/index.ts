b40e62e6b2b9af30386969ba12d39b1d169e2651
Sheldon Kwok <me@sheldonk.com>
2018-07-18 22:43:00 -0700

    Init bot login

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi');
+import * as Discord from 'discord.js';
+const config = require('../config.json');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('ready');
+});
+
+client.login(config.discord.token);
