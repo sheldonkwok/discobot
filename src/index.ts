73a38ffff20ee2a166380c2c00ab1356dd942de1
Sheldon Kwok <me@sheldonk.com>
2018-07-19 00:28:46 -0700

    Speak username or nickname on voice channel join

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,47 @@
+import * as fs from 'mz/fs';
 import * as Discord from 'discord.js';
-const config = require('../config.json');
 
+import * as tts from './tts';
+
+const config = require('../config.json');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+const VOICE_CHANNEL_ID: string = config.discord.voiceChannelID;
+
+function exit() {
+  console.warn('Unintentional exit');
+  console.warn(arguments);
+  process.exit(1);
+}
+
+function matchVoiceChannel(c: Discord.Channel): boolean {
+  return c.type === 'voice' && c.id === VOICE_CHANNEL_ID;
+}
+
+client.on('ready', async () => {
+  const channel = client.channels.find(matchVoiceChannel) as Discord.VoiceChannel;
+  if (!channel) throw new Error('Unable to join voice channel');
+
+  const voiceConn = await channel.join();
+
+  client.on('voiceStateUpdate', async (_, member) => {
+    if (member.voiceChannelID === null) return;
+
+    const username = member.nickname || member.user.username;
+    const file = `/tmp/${username}.mp3`;
+
+    if (!(await fs.exists(file))) await tts.create(username, file);
+
+    const dispatcher = voiceConn.playFile(file);
+    dispatcher.on('end', () => dispatcher.end());
+  });
+
   console.log('ready');
 });
 
+client.on('error', exit);
 client.login(config.discord.token);
+
+process.on('SIGINT', () => {
+  client.destroy();
+});
--- src/tts.ts
+++ src/tts.ts
@@ -4,13 +4,32 @@ import * as qs from 'querystring';
 
 export const HOST = 'translate.google.com';
 
+const BASE_QS = {
+  ie: 'UTF-8',
+  total: 1,
+  idx: 0,
+  textlen: 32,
+  client: 'tw-ob',
+  tl: 'En-gb',
+};
+
 export async function create(username: string, filename: string): Promise<void> {
-  const qStr = qs.stringify({ client: 'tw-obj', tl: 'En-gb', q: username });
-  const fileStream = fs.createWriteStream(filename);
-  const opts = { hostname: HOST, path: `/translate_tts?${qStr}` };
+  const qStr = qs.stringify({ ...BASE_QS, q: username });
+  const opts = {
+    hostname: HOST,
+    path: `/translate_tts?${qStr}`,
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    },
+  };
 
   return new Promise<void>((resolve, rej) => {
     http.get(opts, response => {
+      const code = response.statusCode;
+      if (code !== 200) return rej(new Error(`Unable to create tts code: ${code}`));
+
+      const fileStream = fs.createWriteStream(filename);
       response
         .pipe(fileStream)
         .on('close', () => resolve())
