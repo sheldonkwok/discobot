caec8b5af747aebf443fd2dda8ece1602d7b6230
Sheldon Kwok <me@sheldonk.com>
2018-08-30 20:44:43 -0700

    Handle reconnecting

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const config = require('../config.json');
 const client = new Discord.Client();
 
 const VOICE_CHANNEL_ID: string = config.discord.voiceChannelID;
+const VOICE_RECONNECT_DELAY = 2 * 60 * 60 * 1000;
 
 function exit() {
   console.warn('Unintentional exit');
@@ -17,21 +18,34 @@ function matchVoiceChannel(c: Discord.Channel): boolean {
   return c.type === 'voice' && c.id === VOICE_CHANNEL_ID;
 }
 
+async function handleVoiceState(
+  voiceConn: Discord.VoiceConnection,
+  oldMember: Discord.GuildMember,
+  newMember: Discord.GuildMember,
+): Promise<void> {
+  if (oldMember.voiceChannelID !== null || newMember.voiceChannelID === null) return;
+
+  const username = newMember.nickname || newMember.user.username;
+  const file = await tts.get(newMember.id, username);
+
+  const dispatcher = voiceConn.playFile(file);
+  dispatcher.on('end', () => dispatcher.end());
+}
+
 client.on('ready', async () => {
   const channel = client.channels.find(matchVoiceChannel) as Discord.VoiceChannel;
   if (!channel) throw new Error('Unable to join voice channel');
 
-  const voiceConn = await channel.join();
+  let voiceConn = await channel.join();
 
   client.on('voiceStateUpdate', async (oldMember, newMember) => {
-    if (oldMember.voiceChannelID !== null || newMember.voiceChannelID === null) return;
-
-    const username = newMember.nickname || newMember.user.username;
-    const file = await tts.get(newMember.id, username);
-
-    const dispatcher = voiceConn.playFile(file);
-    dispatcher.on('end', () => dispatcher.end());
+    await handleVoiceState(voiceConn, oldMember, newMember);
   });
+
+  setInterval(async () => {
+    voiceConn.disconnect();
+    voiceConn = await channel.join();
+  }, VOICE_RECONNECT_DELAY);
 
   console.log('ready');
 });
