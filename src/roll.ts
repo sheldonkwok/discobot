8c5898f356b32f7d63a3b8bb99e2dcc6f1ab7199
wrthomps <relambrien@gmail.com>
2020-06-06 14:52:20 -0700

    Prefer default to 100

--- src/roll.ts
+++ src/roll.ts
@@ -6,16 +6,8 @@ const DEFAULT_MAX = 100;
 export async function run(msg: Discord.Message): Promise<void> {
   const { channel, content, author } = msg;
 
-  var match = content.match(ROLL_MAX_REGEX)?.groups?.max;
-  var rollMax;
-  if (match) {
-    rollMax = parseInt(match, 10);
-    if (rollMax < 1) {
-      rollMax = 1;
-    }
-  } else {
-    rollMax = DEFAULT_MAX;
-  }
+  const match = content.match(ROLL_MAX_REGEX)?.groups?.max;
+  const rollMax = match && parseInt(match, 10) > 0 ? parseInt(match, 10) : DEFAULT_MAX;
   const result = Math.floor(Math.random() * rollMax) + 1;
   await channel.send(`${author.username} rolls ${result} (1-${rollMax})`);
 }
